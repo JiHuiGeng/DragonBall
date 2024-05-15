8d58975c8e0793499a3d8302999bd843c5069c0c
gengjihui <gengjihui_maya@163.com>
2024-05-15 16:13:24 +0800

    Initial commit

---
+++ src/main/java/com/cloudinary/dragonball/DragonBallApplication.java
@@ -0,0 +1,13 @@
+package com.cloudinary.dragonball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonBallApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DragonBallApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/cloudinary/dragonball/DragonBallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudinary.dragonball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DragonBallApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
