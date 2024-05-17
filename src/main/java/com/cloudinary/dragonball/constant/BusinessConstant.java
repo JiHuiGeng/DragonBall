f24816136f4eedef6770e5c2c41ccc6c6e6c9b1c
gengjihui <gengjihui_maya@163.com>
2024-05-17 14:00:30 +0800

    增加系统常量类及业务常量类

---
+++ src/main/java/com/cloudinary/dragonball/constant/BusinessConstant.java
@@ -0,0 +1,10 @@
+package com.cloudinary.dragonball.constant;
+
+/**
+ * @Author: Jihui Geng
+ * @Date: 2024/5/15 16:22
+ * @Description: 业务常量
+ */
+public class BusinessConstant {
+
+}
---
+++ src/main/java/com/cloudinary/dragonball/constant/SystemConstant.java
@@ -0,0 +1,9 @@
+package com.cloudinary.dragonball.constant;
+
+/**
+ * @Author: Jihui Geng
+ * @Date: 2024/5/15 16:23
+ * @Description: 系统级常量
+ */
+public class SystemConstant {
+}
