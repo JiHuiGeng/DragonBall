8d1d49224c65ff4a75c3dc2dc87af701c2ccd8ff
gengjihui <gengjihui_maya@163.com>
2024-05-20 09:13:41 +0800

    增加mapper包扫描注解

--- src/main/java/com/cloudinary/dragonball/DragonBallApplication.java
+++ src/main/java/com/cloudinary/dragonball/DragonBallApplication.java
@@ -2,8 +2,10 @@ package com.cloudinary.dragonball;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(value = "com.cloudinary.dragonball.mapper")
 public class DragonBallApplication {
 
   public static void main(String[] args) {
